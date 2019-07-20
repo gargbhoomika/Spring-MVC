ec0b524922743f9642dc5f269f387f2ea0d77eb8
Bhoomika Garg <bhoomiika.garg@gmail.com>
2019-07-21 03:05:49 +0530

    ModelAndView Class

--- src/main/java/garg/bhoomika/AddController.java
+++ src/main/java/garg/bhoomika/AddController.java
@@ -1,13 +1,26 @@
 package garg.bhoomika;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AddController {
 	@RequestMapping("/add")
-	public void add()
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response)
 	{
-	    System.out.println("I am here");
+		int i = Integer.parseInt(request.getParameter("t1"));
+		int j = Integer.parseInt(request.getParameter("t2"));
+		int k = i+j;
+		
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("display.jsp");
+		mv.addObject("result",k);
+		
+		return mv;
 	}
-
+	
 }
