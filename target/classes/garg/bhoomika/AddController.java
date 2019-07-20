ddc35260f0f2a9736f6fd2c97c99f33b9034e046
Bhoomika Garg <bhoomiika.garg@gmail.com>
2019-07-20 18:09:48 +0530

    Dispatcher Servlet

--- target/classes/garg/bhoomika/AddController.java
+++ target/classes/garg/bhoomika/AddController.java
@@ -1,7 +1,12 @@
 package garg.bhoomika;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AddController 
 {
+	@RequestMapping("/add")
 	public void add()
 	{
 		System.out.println("I am here");
